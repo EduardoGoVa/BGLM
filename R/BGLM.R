ca80a4d2146a0a0e03e5f769bf2ca01bfca35290
EduardoGoVa <eduardo.gova1022@gmail.com>
2026-07-19 21:28:26 -0600

    Update NB model

--- R/BGLM.R
+++ R/BGLM.R
@@ -3933,14 +3933,10 @@ MTME<-function(
 	if(response_type == "NB"){
       rvNB = sweep(x=rv,MARGIN=2,STATS=log(r),FUN="+")
       y_tr = exp(rvNB)
-	  print(y_tr)
       if(nNa>0){
 		size_vec <- rep(r, each = nNa)
 		rp = y_tr[!NoWhichNa,]
-		print(size_vec)
-		print(rp)
         yStar[!NoWhichNa,] = matrix(rnbinom(n=nNa*ntraits,size=size_vec,mu=as.vector(rp)),nrow=nNa,ncol=ntraits)
-		print(yStar[!NoWhichNa,])
       }
       loglik = fllp_NB_multi(rv=rv[NoWhichNa,],y=y[NoWhichNa,],r=r,ntraits = ntraits)
     }
